15f1e04786b948d45839a320116c49da60cda348
Wnagfeng <2712442320@qq.com>
2024-03-03 14:41:29 +0800

    完成项目初始化

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,9 @@
 import React from 'react'
-import logo from './logo.svg'
-import Hello from './components/Helloword'
-import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Hello message="我是自定义组件"></Hello>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>你好</h1>
     </div>
   )
 }
--- src/components/Helloword.tsx
+++
@@ -1,7 +0,0 @@
-interface IHelloword {
-  message: string
-}
-const Hello = (props: IHelloword) => {
-  return <h1>{props.message}</h1>
-}
-export default Hello
